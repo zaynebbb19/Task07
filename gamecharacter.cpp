481ee10c758ba297a6760a061406d3f92672baae
zaynebbb19 <zainabzahid704@gmail.com>
2024-09-19 07:32:06 -0700

    Add files via upload

---
+++ gamecharacter.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main()
+{
+cout<<"               ________"<<endl;
+cout<<"              /        \\"<<endl;
+cout<<"             /-----------\\"<<endl;
+cout<<"              | |     \\ \\"<<endl;
+cout<<"              |_| @    \\_\\"<<endl;                
+cout<<"             ----:   __,  )"<<endl;
+cout<<"          /  /   :_______/"<<endl;
+cout<<"         /  /   /###/   |#|"<<endl;         
+cout<<"        /  /   /###/    |#|___: )"<<endl;
+cout<<"       (__:   /###/_____|#|____/"<<endl;
+cout<<"               /    /\\   \\"<<endl;
+cout<<"              /    /  \\   \\"<<endl;
+cout<<"             /    /    \\   \\"<<endl;
+cout<<"            /    /      \\   \\"<<endl;
+cout<<"           ######        #####";
+}       
